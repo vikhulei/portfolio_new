f5db7cc6f4c8a53d1115ca007d9bd3efd951a8ac
vikhulei <vikhulei@gmail.com>
2022-10-15 13:15:00 +0100

    Contacts section completed

--- src/sections/09_contacts/Contacts.js
+++ src/sections/09_contacts/Contacts.js
@@ -1,8 +1,24 @@
-import {Wrapper} from "./ContactsStyle"
+import { Wrapper, LargeHeading, Footer, CopyrightNameContainer, CopyrightText, NameText, PortfolioContainer, CheckPortfolioText, PortfolioAddress, ContactsContainer, PhoneNumber, EmailAddress } from "./ContactsStyle"
 
 const Contacts = () => {
   return (
-    <Wrapper>Contacts</Wrapper>
+    <Wrapper>
+      <LargeHeading>Let’s get in touch!</LargeHeading>
+      <Footer>
+        <CopyrightNameContainer>
+          <CopyrightText>© 2022</CopyrightText>
+          <NameText>Viktor Hulei</NameText>
+        </CopyrightNameContainer>
+        <PortfolioContainer>
+          <CheckPortfolioText>Check the code for this portfolio</CheckPortfolioText>
+          <PortfolioAddress href="https://github.com/vikhulei/portfolio_new">https://github.com/vikhulei/portfolio_new</PortfolioAddress>
+        </PortfolioContainer>
+        <ContactsContainer>
+          <PhoneNumber>+447706 204681</PhoneNumber>
+          <EmailAddress href="mailto:vikhulei@gmail.com">vikhulei@gmail.com</EmailAddress>
+        </ContactsContainer>
+      </Footer>
+    </Wrapper>
   )
 }
 
--- src/sections/09_contacts/ContactsStyle.js
+++ src/sections/09_contacts/ContactsStyle.js
@@ -1,8 +1,65 @@
 import styled from "styled-components"
-import { WrapperStyled } from "../../components/ui/StyledComponents"
+import { WrapperStyled, LargeHeadingStyled } from "../../components/ui/StyledComponents"
+import { colors } from "../../components/ui/Colors"
+
+const {middle} = colors;
+
 
 const Wrapper = styled(WrapperStyled) `
+    height: 400px;
+    justify-content: space-between;
+`
+
+const LargeHeading = styled(LargeHeadingStyled) `
+    font-size: 8rem;
+`
+
+const Footer = styled.div `
+    background-color: ${middle};
+    width: 100%;
+    height: 180px;
+    padding: 0 80px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    line-height: 1.5;
+`
+
+const CopyrightNameContainer = styled.div `
+    
+`
+
+const CopyrightText = styled.p `
+`
+const NameText = styled.p `
 
 `
 
-export {Wrapper}
+
+const PortfolioContainer = styled.div `
+    font-style: italic;
+    text-align: center;
+`
+
+const CheckPortfolioText = styled.p `
+    font-weight: bold;
+`
+
+const PortfolioAddress = styled.a `
+
+`
+
+const ContactsContainer = styled.div `
+    text-align: right;
+`
+
+const PhoneNumber = styled.p `
+
+`
+
+const EmailAddress = styled.a `
+
+`
+
+
+export { Wrapper, LargeHeading, Footer, CopyrightNameContainer, CopyrightText, NameText, PortfolioContainer, CheckPortfolioText, PortfolioAddress, ContactsContainer, PhoneNumber, EmailAddress }
