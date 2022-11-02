51e98c99d8bdcb17f54d507a65f396bc873af064
vikhulei <vikhulei@gmail.com>
2022-11-02 10:19:41 +0000

    custom animation hook implemented

--- src/util/showingPointFunction.js
+++
@@ -1,4 +0,0 @@
-export const showingPointFunction = {
-    showingPoint: () => (window.innerHeight - (window.innerHeight*10/100)),
-    elementTop: (elementName) => elementName.getBoundingClientRect().top
-}
